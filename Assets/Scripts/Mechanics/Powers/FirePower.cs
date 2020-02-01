ca99bed3e48d14d90f01467deef490b0f7d5b008
Tom Kattouw <tomkattouw@hotmail.com>
2020-02-01 13:09:17 +0100

    Added fire!

---
+++ Assets/Scripts/Mechanics/Powers/FirePower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePower : Power
+{
+	public override void Init()
+	{
+		base.Init();
+	}
+
+	public override void NullifyPower()
+	{
+		base.NullifyPower();
+	}
+}
---
+++ Assets/Scripts/Mechanics/Powers/ShootFire.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class ShootFire : MonoBehaviour
+{
+	[SerializeField] GameObject _firePrefab;
+	[SerializeField] float _offset = 0.5f;
+	[SerializeField] float _projectileSpeed = 5;
+
+	SpriteRenderer _character;
+
+	public Action<float> OnPowerUsage;
+
+	private void Start()
+	{
+		_character = GetComponent<SpriteRenderer>();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+			Shoot();
+	}
+
+	public void Shoot()
+	{
+		GameObject projectile = Instantiate(_firePrefab, StartingPoint(), Quaternion.identity) as GameObject;
+		Fire fire = projectile.GetComponent<Fire>();
+		fire.Movespeed = Movespeed();
+	}
+
+	Vector2 StartingPoint()
+	{
+		float offset = 0;
+
+		if (_character.flipX == false)
+			offset = _offset;
+		else
+			offset = -_offset;
+
+		Vector2 position = (Vector2)transform.position + new Vector2(offset, 0);
+
+		return position;
+	}
+
+	float Movespeed()
+	{
+		float speed = 0;
+
+		if (_character.flipX == false)
+			speed = _projectileSpeed;
+		else
+			speed = -_projectileSpeed;
+
+		return speed;
+	}
+}
